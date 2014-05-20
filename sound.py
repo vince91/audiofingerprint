e92d19cd4c7e4364d16ae354080012abe69ed306
Vincent Timofti <vincent.timofti@gmail.com>
2014-05-20 10:09:51 +0200

    minor corrections for class Sound

--- sound.py
+++ sound.py
@@ -1,10 +1,13 @@
 import pyaudio
+from array import array
+import sys
+import numpy
 
 class Sound:
 	
 	CHUNK = 1024
 	FORMAT = pyaudio.paInt16
-	CHANNELS = 2
+	CHANNELS = 1
 	RATE = 44100
 
 	def __init__(self, duration = 5):
@@ -16,17 +19,26 @@ class Sound:
 		"""
 		print('Recording a %d seconds sound' % self.duration);
 
-		self.samples = []
+		self.samples = array('h')
 
 		p = pyaudio.PyAudio();
 		stream = p.open(format = self.FORMAT, channels = self.CHANNELS, rate = self.RATE, input = True, frames_per_buffer = self.CHUNK)
 
 		for i in range(0, int(self.duration * self.RATE / self.CHUNK)):
-			self.samples.append(stream.read(self.CHUNK))
+			data = array('h', stream.read(self.CHUNK))
+			if sys.byteorder == 'big':
+				data.byteswap()
+			self.samples.extend(data)
 
 		stream.stop_stream()
 		stream.close()
 		p.terminate()
+
+
+	def save(self):
+
+		numpy.savetxt("foo.csv", self.samples, delimiter=",")
+
 
 
 
